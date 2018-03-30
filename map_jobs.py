2c34af92e9b61312903d6d269cb2fefc7bfb8e00
David Udwin <david.udwin@gmail.com>
2018-03-29 23:07:05 -0400

    Create skeleton of angular application

---
+++ map_jobs.py
@@ -0,0 +1,26 @@
+import os
+import re
+# We'll render HTML templates and access data sent by POST using the request object from flask. Redirect and url_for
+# will be used to redirect the user once the upload is done and send_from_directory will help us to send/show on the
+# browser the file that the user just uploaded
+from flask import Flask, render_template, request, jsonify, redirect, session, url_for, send_from_directory
+from flask_bower import Bower
+from werkzeug import secure_filename
+
+# Triangle is very important and fixes the Angular / Flask {} issue; 
+from flask_triangle import Triangle
+from flask_compress import Compress
+
+# Initialize the Flask application
+app = Flask(__name__)
+Bower(app)
+Triangle(app)
+Compress(app)
+
+# Home page
+@app.route('/')
+def main():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081,debug=True)
---
+++ static/app.js
@@ -0,0 +1,6 @@
+
+var map_of_jobs = angular.module('map_of_jobs', []);
+
+map_of_jobs.controller(map_of_jobs_controller, function($scope, $http) {
+	
+});
