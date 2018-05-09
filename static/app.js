ae65d1f513f80e81c864c85e69d068ffed73a3ab
Xavier Harris <xharris1@umbc.edu>
2018-05-09 12:16:11 -0400

    added search form and results container

--- static/app.js
+++ static/app.js
@@ -12,12 +12,27 @@ map_of_jobs.controller('map_of_jobs_controller', function($scope, $http, $window
 	$scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyAnGj_CJuLQjEjd94i0MOvQVB4FDLRpdec";
 
 	$scope.populateMap = function(){
-
+		console.log('hi')
 	};
 
 	$http({url:'map_data', method:"GET"}).then(function(map_data){
 		console.log(map_data);
 	});
-});
 
+	$scope.search = {
+		submit: function() {
+			var results = [
+				{address: '50 bobby lane', zipcode: 12345},
+				{address: '51 bobby lane', zipcode: 12345},
+				{address: '52 bobby lane', zipcode: 12345}
+			];
+
+			$scope.search.results = results;
+
+
+			// toggle results div class
+			$scope.showing_results = true;
+		}
+	}
+})
 
