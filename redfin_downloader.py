382d063b717e3be029b8bb34349d7b8ca65ac4b1
Hannah Kiesel <hmkiesel@gmail.com>
2018-05-14 12:47:56 -0400

    added latitude and longitude

--- redfin_downloader.py
+++ redfin_downloader.py
@@ -7,13 +7,13 @@ import builtins
 builtins.unicode = str
 from lxml import html
 import unicodecsv as csv
-import re
 
 import csv
 import time
 import glob
 import fileinput
 import requests
+from requests.exceptions import ConnectionError
 from bs4 import BeautifulSoup
 import re
 from flask import Flask, render_template, request, jsonify, redirect, session, url_for, send_from_directory, Response
@@ -104,86 +104,109 @@ def clean_folder():
 
 
 def parse(location,rating):
-    url = ("https://www.zillow.com/homes/for_sale/%s/%sb_sch"%(location,rating))
-
-    for i in range(5):
-        # try:
-        headers = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'accept-encoding': 'gzip, deflate, sdch, br',
-            'accept-language': 'en-GB,en;q=0.8,en-US;q=0.6,ml;q=0.4',
-            'cache-control': 'max-age=0',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
-        }
-        response = requests.get(url, headers=headers)
-        print(response.status_code)
-        parser = html.fromstring(response.text)
-        search_results = parser.xpath("//div[@id='search-results']//article")
-        properties_list = []
-
-        for properties in search_results:
-            raw_address = properties.xpath(".//span[@itemprop='address']//span[@itemprop='streetAddress']//text()")
-            raw_city = properties.xpath(".//span[@itemprop='address']//span[@itemprop='addressLocality']//text()")
-            raw_state = properties.xpath(".//span[@itemprop='address']//span[@itemprop='addressRegion']//text()")
-            raw_postal_code = properties.xpath(".//span[@itemprop='address']//span[@itemprop='postalCode']//text()")
-            raw_price = properties.xpath(".//span[@class='zsg-photo-card-price']//text()")
-            raw_info = properties.xpath(".//span[@class='zsg-photo-card-info']//text()")
-            raw_broker_name = properties.xpath(".//span[@class='zsg-photo-card-broker-name']//text()")
-            url = properties.xpath(".//a[contains(@class,'overlay-link')]/@href")
-            raw_title = properties.xpath(".//h4//text()")
-
-            address = ' '.join(' '.join(raw_address).split()) if raw_address else None
-            city = ''.join(raw_city).strip() if raw_city else None
-            state = ''.join(raw_state).strip() if raw_state else None
-            postal_code = ''.join(raw_postal_code).strip() if raw_postal_code else None
-            price = ''.join(raw_price).strip() if raw_price else None
-            if price:
-                price = price.split('$')[1]
-                if len(price.split(',')) == 2:
-                    price1, price2 = price.split(',') if price else None
-                    price = int(price1) * 1000 + float(price2[:3]) if price1 and price2 else None
-                elif len(price.split(','))==3:
-                    price1, price2, price3 = price.split(',') if price else None
-                    price = int(price1) * 1000000 + float(price2[:3]) * 1000 + float(price2[:3]) if price1 and price2 and price3 else None
-                else:
-                    if price[-1] == 'K':
-                        price = int(price[:-1])*1000
-                    price = int(price)
-            info = ' '.join(' '.join(raw_info).split()).replace(u"\xb7", ',')
-            row = info.split(' ')
-            beds = row[0] if row[0] != "--" else None
-            baths = row[3] if row[3] != "--" else None
-            size = row[6] if row[6] != "--" else None
-            if size == '--' or size is None:
-                size = None
-            else:
-                if len(size.split(',')) > 1:
-                    size = "%s%s"%(size.split(',')[0],size.split(',')[1])
-                size = float(size)
-            broker = ''.join(raw_broker_name).strip() if raw_broker_name else None
-            title = ''.join(raw_title) if raw_title else None
-            print(title)
-            type = title.split(' ')[0] if title else None
-            if type == 'For':
-                type = 'unknown'
-            property_url = "https://www.zillow.com" + url[0] if url else None
-            is_forsale = properties.xpath('.//span[@class="zsg-icon-for-sale"]')
-            properties = {
-                'address': address,
-                'city': city,
-                'state': state,
-                'postal_code': postal_code,
-                'price': price,
-                'beds': beds,
-                'baths': baths,
-                'size': size,
-                'url': property_url,
-                'type': type
+    link = ("https://www.zillow.com/homes/for_sale/%s/prcu%sb_sch"%(location,rating))
+    error = False
+    properties_list = []
+    for p in range(2):
+        if p > 0:
+            link = ("https://www.zillow.com/homes/for_sale/%s/%s_p/prcu%sb_sch"%(location,str(p+1),rating))
+        print(link)
+        for i in range(1):
+            # try:
+            headers = {
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'accept-encoding': 'gzip, deflate, sdch, br',
+                'accept-language': 'en-GB,en;q=0.8,en-US;q=0.6,ml;q=0.4',
+                'cache-control': 'max-age=0',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
             }
-            if is_forsale:
-                properties_list.append(properties)
-        return properties_list
+            time.sleep(1)
+            try:
+                response = requests.get(link, headers=headers)
+            except ConnectionError:
+                print("error at page: " + str(p))
+                error = True
+                break
+            parser = html.fromstring(response.text)
+            search_results = parser.xpath("//div[@id='search-results']//article")
+            print(search_results)
+            for properties in search_results:
+                raw_address = properties.xpath(".//span[@itemprop='address']//span[@itemprop='streetAddress']//text()")
+                raw_city = properties.xpath(".//span[@itemprop='address']//span[@itemprop='addressLocality']//text()")
+                raw_state = properties.xpath(".//span[@itemprop='address']//span[@itemprop='addressRegion']//text()")
+                raw_postal_code = properties.xpath(".//span[@itemprop='address']//span[@itemprop='postalCode']//text()")
+                raw_price = properties.xpath(".//span[@class='zsg-photo-card-price']//text()")
+                raw_info = properties.xpath(".//span[@class='zsg-photo-card-info']//text()")
+                raw_broker_name = properties.xpath(".//span[@class='zsg-photo-card-broker-name']//text()")
+                url = properties.xpath(".//a[contains(@class,'overlay-link')]/@href")
+                raw_title = properties.xpath(".//h4//text()")
+                latitude = properties.xpath(".//span[@itemprop='geo']//meta[@itemprop='latitude']//text()")
+                longitude = properties.xpath(".//span[@itemprop='geo']//meta[@itemprop='longitude']//text()")
+                latitude = float(''.join(latitude).strip()) if latitude else None
+                longitude = float(longitude) if longitude else None
+                address = ' '.join(' '.join(raw_address).split()) if raw_address else None
+                city = ''.join(raw_city).strip() if raw_city else None
+                state = ''.join(raw_state).strip() if raw_state else None
+                postal_code = ''.join(raw_postal_code).strip() if raw_postal_code else None
+                price = ''.join(raw_price).strip() if raw_price else None
+                if price:
+                    price = price.split('$')[1]
+                    if len(price.split(',')) == 2:
+                        price1, price2 = price.split(',') if price else None
+                        price = int(price1) * 1000 + float(price2[:3]) if price1 and price2 else None
+                    elif len(price.split(','))==3:
+                        price1, price2, price3 = price.split(',') if price else None
+                        price = int(price1) * 1000000 + float(price2[:3]) * 1000 + float(price2[:3]) if price1 and price2 and price3 else None
+                    else:
+                        if price[-1] == 'K':
+                            price = float(price[:-1])*1000
+                        price = int(price)
+                info = ' '.join(' '.join(raw_info).split()).replace(u"\xb7", ',')
+                row = info.split(' ')
+                beds = None
+                baths = None
+                size = None
+                if len(row) > 1 and row[1] == "bds":
+                    beds = row[0] if row[0] != "--" else None
+                if len(row) > 4 and row[4] == "ba":
+                    baths = row[3] if row[3] != "--" else None
+                if len(row) > 7 and row[7] == "sqft":
+                    size = row[6] if row[6] != "--" else None
+                if size == '--' or size is None:
+                    size = None
+                else:
+                    if len(size.split(',')) > 1:
+                        size = "%s%s"%(size.split(',')[0],size.split(',')[1])
+                    if size[-1] == '+':
+                        size = size[:-1]
+                    size = float(size)
+                title = ''.join(raw_title) if raw_title else None
+                type = title.split(' ')[0] if title else None
+                if type == 'For' or type == 'Auction' or type == 'New':
+                    type = 'unknown'
+                property_url = "https://www.zillow.com" + url[0] if url else None
+                is_forsale = properties.xpath('.//span[@class="zsg-icon-for-sale"]')
+                properties = {
+                    'address': address,
+                    'city': city,
+                    'state': state,
+                    'postal_code': postal_code,
+                    'price': price,
+                    'beds': beds,
+                    'baths': baths,
+                    'size': size,
+                    'url': property_url,
+                    'type': type,
+                    'lat': latitude,
+                    'long': longitude
+                }
+                print(properties)
+                if is_forsale:
+                    properties_list.append(properties)
+        if error:
+            break
+    return properties_list
     # except:
     # 	print ("Failed to process the page",url)
 
@@ -250,19 +273,19 @@ def filter_data(minBedrooms, minBathrooms, propertyTypes, minHouseSize,minPrice,
         keep = True
 
         if row['size'] and float(row['size']) < float(minHouseSize):
-            print('size')
+            print('s')
             keep = False
         elif row['beds'] and int(row['beds']) < int(minBedrooms):
-            print('bedrooms')
+            print('bd')
             keep = False
         elif row['baths'] and float(row['baths']) < float(minBathrooms):
-            print('bathrooms')
+            print('bt')
             keep = False
         elif row['type'] not in propertyTypes:
-            print('type')
+            print('t')
             keep = False
         elif row['price'] != "--" and (float(row['price']) > float(maxPrice) or float(row['price']) < float(minPrice)):
-            print('price')
+
             keep = False
 
         else:
@@ -319,43 +342,38 @@ def get_data():
     #while(request.args.get('data') == None):
     #    time.sleep(0.5)
 
-    print(request.get_data())
-    data = re.search(r"\{\"location\":\"(\w+)\",\"townhouse\":(true|false|0),\"house\":(true|false|0),\"condo\":(true|false|0),\"rating\":(\d+),\"price\":\"([\d,]+)\",\"beds\":(\d+),\"baths\":([\d\.]+),\"size\":(\d+)\}", str(request.get_data())).groups()
-
-    minBedrooms = int(data[6])  # minimum number of bedrooms
-    minBathrooms = float(data[7])  # minimum number of bathrooms
-
-    # property types requested
-    true_values = ['true','True','1']
+    data = str(request.get_data()).split(',')
+    minBedrooms = data[7].split(':')[1]  # minimum number of bedrooms
+    minBathrooms = data[8].split(':')[1]  # minimum number of bathrooms
+    types = [data[1].split(':')[1],data[2].split(':')[1],data[3].split(':')[1]]  # property types requested
     propertyTypes = []
-    if data[1] in true_values:
+    if types[0] == 'true':
         propertyTypes.append('Townhouse')
-    if data[2] in true_values:
+    if types[1] == 'true':
         propertyTypes.append('House')
-    if data[3] in true_values:
+    if types[2] == 'true:':
         propertyTypes.append('Condo')
-
-    minHouseSize = float(data[8])  # minimum house size in sq ft
-    location = str(data[0])
-    rating = int(data[4])
-    price = str(data[5]).split(',')  # min,max house price
+    minHouseSize = data[9].split(':')[1].split('}')[0]  # minimum house size in sq ft
+    price = (data[5]+','+data[6]).split(':')[1]  # house price range
+    location = data[0].split(':')[1]
+    rating = data[4].split(':')[1]
+    minPrice = maxPrice = 0
+    #price = price.split(':')[1]
+    price = price.split('"')[1]
+    price = price.split(',')
     minPrice, maxPrice = int(price[0]), int(price[1])
 
     # clean_folder()  # remove old csv files
-    # location = 'Fort Smith, AR'
-    # rating = 4
-    # # sort = args.sort
     # print("Fetching data for %s" % (location))
     scraped_data = parse(location, rating)
     # print("Writing data to output file")
     with open("_all_properties.csv", 'w')as csvfile:
-        fieldnames = ['type', 'address', 'city', 'state', 'postal_code', 'price', 'beds', 'baths', 'size', 'url']
+        fieldnames = ['type', 'address', 'city', 'state', 'postal_code', 'price', 'beds', 'baths', 'size', 'url', 'lat', 'long']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for row in scraped_data:
             writer.writerow(row)
     data = filter_data(minBedrooms, minBathrooms, propertyTypes, minHouseSize, minPrice, maxPrice)
-    print(data)
     return Response(jsonify(data).data)
 
 # Home page
@@ -366,4 +384,4 @@ def main():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081, debug=True)
+    app.run(host='0.0.0.0', port=8082, debug=True)
